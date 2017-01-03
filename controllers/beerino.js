0fb73ab26b3f14de3617d3e7259cd53fa4ead783
Rodrigo Dagnese <dagnese.rodrigo@gmail.com>
2017-01-03 16:41:29 -0200

    feature(beerino) save beerino

--- controllers/beerino.js
+++ controllers/beerino.js
@@ -1,5 +1,30 @@
 module.exports = function (app) {
-    
+    app.get('/beerino/:beerinoId', function(req, res) {
+        var connection = app.repository.connectionFactory();
+        var beerinoRepository = new app.repository.beerinoRepository(connection);
+        
+        beerinoRepository.get(req.params.beerinoId, function(error, result) {
+            if (error) {
+                res.status(500).send(error);
+            } else {
+                res.status(201).json(result);
+            }
+        });
+    });
+
+    app.post('/beerino', function (req, res) {
+        var connection = app.repository.connectionFactory();
+        var beerinoRepository = new app.repository.beerinoRepository(connection);
+        var beerino = req.body;
+        console.log(beerino);
+        beerinoRepository.save(beerino, function (error, result) {
+            if (error) {
+                res.status(500).send(error);
+            } else {
+                res.status(201).json(result);
+            }
+        });
+    });
 
     // app.post("/beerino/register/:userId/:beerinoUserId", function (oRequest, oResponse) {
     //     var oConnection = app.repository.connectionFactory();
