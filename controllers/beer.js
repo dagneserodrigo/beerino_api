d8acac56b5d0ac448e4356e5874b3e1719fc5805
Rodrigo Dagnese <dagnese.rodrigo@gmail.com>
2017-01-04 10:30:26 -0200

    feature(beer) paging list beer

--- controllers/beer.js
+++ controllers/beer.js
@@ -25,4 +25,21 @@ module.exports = function(app) {
             }
         });
     });
+
+    app.get('beers/:page/:limit', function(req, res) {
+        var connection = app.repository.connectionFactory();
+        var beerRepository = new app.repository.beerRepository(connection);
+        var pagingConfig = {
+            page: req.params.page,
+            limite: req.params.limit
+        };
+
+        beerRepository.list(pagingConfig, function(error, result) {
+            if (error) {
+                res.status(500).send(error);
+            } else {
+                res.status(201).send(result);
+            }
+        });
+    });
 };
--- repository/beerRepository.js
+++ repository/beerRepository.js
@@ -3,11 +3,15 @@ function beerRepository (connection) {
 }
 
 beerRepository.prototype.get = function (beerId, callback) {
-    this._connection.query("SELECT beerId, name, drescription, recipe, public, userId from beer WHERE beerId = ?", beerId, callback);
+    this._connection.query("SELECT beerId, name, drescription, recipe, public, userId FROM beer WHERE beerId = ?", beerId, callback);
 };
 
 beerRepository.prototype.save = function (beer, callback) {
     this._connection.query("INSERT INTO beer SET ? ON DUPLICATE KEY UPDATE ?", [beer, beer], callback);
+};
+
+beerRepository.prototype.list = function (pagingConfig, callback) {
+    this._connection.query("SELECT beerId, name, drescription, recipe, public, userId from beer LIMIT ?,?", [pagingConfig.page, pagingConfig.limit], callback);
 };
 
 module.exports = function () {
